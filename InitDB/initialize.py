ca3050bd3e1bc979ec13ea189ab2facf29563d2f
alexs2112 <alexander.stevenson@ucalgary.ca>
2022-11-26 11:44:32 -0700

    minor fix to initialize script

--- InitDB/initialize.py
+++ InitDB/initialize.py
@@ -29,7 +29,7 @@ def establish_connection():
     return db
 
 if __name__ == "__main__":
-    if (len(sys.argv) < 1 or "help" == sys.argv[1]):
+    if (len(sys.argv) < 2 or "help" == sys.argv[1]):
         help()
         exit(0)
 
