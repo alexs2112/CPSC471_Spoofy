59dae817f1757a4f529d52afb520e8b89a02dc93
Eric G <65147562+E-Gantz@users.noreply.github.com>
2022-11-30 12:05:06 -0700

    added title, whitespace to error

--- Spoofy/error.php
+++ Spoofy/error.php
@@ -1,10 +1,9 @@
 <html>
     <head>
-        <title>PageTitle - Spoofy</title>
+        <title>error - Spoofy</title>
     </head>
     <body>
         <?php
-            // These include statments are relative to the file you are editing
             include "modules/menubar.php";
             echo "<h1>Error 401 Authorization Required</h1>";
         ?>
