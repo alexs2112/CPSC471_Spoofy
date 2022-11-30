4396eb6d1d0d8a4ad9701915f10b5caed0651978
E-Gantz <65147562+E-Gantz@users.noreply.github.com>
2022-11-30 11:12:14 -0700

    Created error page

---
+++ Spoofy/error.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>PageTitle - Spoofy</title>
+    </head>
+    <body>
+        <?php
+            // These include statments are relative to the file you are editing
+            include "modules/menubar.php";
+            echo "<h1>Error 401 Authorization Required</h1>";
+        ?>
+    </body>
+</html>
