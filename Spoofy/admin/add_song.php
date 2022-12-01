d39f898b90fb2479c7a9d99f6e942269cf9c435c
FGCOrion <47425437+FGCOrion@users.noreply.github.com>
2022-11-30 23:29:45 -0700

    Added song managing to admin

---
+++ Spoofy/admin/add_song.php
@@ -0,0 +1,96 @@
+<?php
+include "../modules/menubar.php";
+include "../modules/mysql_connect.php";
+
+if(!isset($_SESSION)) { session_start(); }
+if (isset($_SESSION["LoggedIn"]) && $_SESSION["LoggedIn"] && $_SESSION["Admin"]){
+
+	// Define variables and initialize with empty values
+	$error_string = "";
+	$title = "";
+	$duration = "";
+	$filepath = "";
+
+	// Processing form data when form is submitted
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+		// Validate title
+		$title = trim($_POST["title"]);
+		if(empty($title)) {
+			$error_string = "Title can't be empty.";
+		} elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["title"]))) {
+			$error_string = "Title can only contain letters, numbers, and underscores.";
+		}
+		
+		// Validate duration
+		$duration = trim($_POST["duration"]);
+		if(empty($duration)) {
+			$error_string = "Duration can't be empty.";     
+		} elseif(!preg_match('/^[0-5][0-9]:[0-5][0-9]:[0-5][0-9]$/', trim($_POST["duration"]))) {
+			$error_string = "Duration should be formatted as 'hh:mm:ss'";
+		}
+		
+		// Validate filepath
+		// TODO: Actual validation checks
+		$filepath = trim($_POST["filepath"]);
+
+		// If there are no errors, insert into the database
+		if(empty($error_string)) {
+			
+			// Prepare an insert statement
+			$sql = "INSERT INTO SONG (Title, Duration, MusicFile) VALUES (?, ?, ?)";
+			$prepare = mysqli_prepare($con, $sql);
+			if($prepare) {
+				
+				// Bind all values
+				$prepare -> bind_param("sss", $title, $duration, $filepath);
+				$prepare -> execute();
+				$result = $prepare -> get_result();
+				
+				// Redirect to login page after registering
+				header("location: manage_songs.php");
+				$prepare -> close();
+			}
+		}
+		
+		// Close connection
+		mysqli_close($con);
+	}
+} else {
+	header("location: ../error.php");
+}
+?>
+
+<html>
+    <head>
+        <title>Register - Spoofy</title>
+    </head>
+    <body>
+        <div class="wrapper">
+        <h2>Add a Song</h2>
+        <p>Fill in song information:</p>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <div class="form-group">
+                    <label>Song Title</label>
+                    <input type="text" name="title" class="form-control" value="<?php echo $title; ?>">
+                </div>    
+                <div class="form-group">
+                    <label>Duration</label>
+                    <input type="text" name="duration" class="form-control" value="<?php echo $duration; ?>">
+                </div>
+                <div class="form-group">
+                    <label>Song File Path</label>
+                    <input type="text" name="filepath" class="form-control" value="<?php echo $filepath; ?>">
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Submit">
+                    <input type="reset" class="btn btn-secondary ml-2" value="Reset">
+                </div>
+                <?php if ($error_string) echo "<p style=\"color:red;\">".$error_string."</p>";?>
+				<button onclick='location.href="add_song.php"' type='button'>
+					Return to Manage Songs
+				</button><br>
+            </form>
+        </div>
+    </body>
+</html>
---
+++ Spoofy/admin/manage_songs.php
@@ -0,0 +1,70 @@
+<?php
+	include "../modules/menubar.php";
+	include "../modules/mysql_connect.php";
+	
+	//links to artists and albums pages
+	
+	//list the songs
+	echo "<h2>Manage Songs:</h2>";
+	
+	echo "<button onclick='location.href=\"add_song.php\"' type='button'>
+		Add Song
+	</button><br>";
+
+	$result = mysqli_query($con, "SELECT * FROM Song");
+	echo "<table border='1'>
+	<tr>
+	<th>ID</th>
+	<th>Title</th>
+	<th>Monthly Plays</th>
+	<th>Artist</th>
+	<th>Album</th>
+	</tr>";
+
+	while($row = mysqli_fetch_array($result)) {
+		// @todo: add Admin to account type
+		echo "<tr>
+		<td>" . $row['SongID'] . "</td>
+		<td>" . $row['Title'] . "</td>";
+		if($row['MonthlyPlays']=== null){
+			echo "<td>0</td>";
+		}
+		else{
+			echo "<td>" . $row['MonthlyPlays'] . "</td>";
+		}
+		//<td><a href='/user/profile.php?UserID= " . $row['UserID'] . "'>View</a></td>
+
+		$sql = "SELECT * FROM WRITES, ARTIST WHERE SongID=? AND WRITES.ArtistID=ARTIST.ArtistID";
+		$prepare = mysqli_prepare($con, $sql);
+		if ($prepare) {
+			$prepare -> bind_param("s", $row['SongID']);
+			$prepare -> execute();
+			$album = $prepare -> get_result();
+			$row2 = mysqli_fetch_array($album);
+			echo"<td>".$row2['Name']."</td>";
+		}
+
+		$sql = "SELECT * FROM ALBUM_CONTAINS, ALBUM WHERE SongID=? AND ALBUM_CONTAINS.AlbumID=ALBUM.AlbumID";
+		$prepare = mysqli_prepare($con, $sql);
+		if ($prepare) {
+			$prepare -> bind_param("s", $row['SongID']);
+			$prepare -> execute();
+			$album = $prepare -> get_result();
+			$row2 = mysqli_fetch_array($album);
+			echo"<td>".$row2['Title']."</td>";
+		}
+		
+		echo "<td><a href='/admin/delete_song.php?SongID= " . $row['SongID'] . "' onclick=\"return confirm('Are you sure?')\";>Delete</a></td>";
+		echo "<td><a href='/admin/clear_monthly.php?SongID= " . $row['SongID'] . "' onclick=\"return confirm('Are you sure?')\";>Reset Monthly Plays</a></td>";
+		"</tr>";
+	}
+	echo "</table>";
+
+	mysqli_close($con);
+?>
+
+<html>
+    <head>
+        <title>Manage Music - Spoofy</title>
+    </head>
+</html>
